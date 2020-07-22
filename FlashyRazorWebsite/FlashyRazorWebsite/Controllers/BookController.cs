c0d9dc976b48879494c07c72c5f10d58f5e80b12
BrandonMefford <B.mefford1997@gmail.com>
2020-07-22 11:20:35 -0500

    Adding my fancy razor website with basic CRUD operations

---
+++ FlashyRazorWebsite/FlashyRazorWebsite/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using FlashyRazorWebsite.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashyRazorWebsite.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+    }
+}
